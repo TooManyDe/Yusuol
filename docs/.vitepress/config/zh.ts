7ba0fa42b7947cdb3cb1edbab22f74b2ddc11ac5
yusuol <jihaoqi@yahoo.com>
2025-07-21 01:23:19 +0800

    Update zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -2,7 +2,7 @@ import { type DefaultTheme, defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Yusuol",
+  title: "冀望",
   description: "",
   lang: "zh-Hans", //语言
 
@@ -14,7 +14,7 @@ export default defineConfig({
    { text: "赞赏", link: "/support-me", activeMatch: '/support-me/' },
     ],
     footer: {
-      message: '© 2024-Present <a href="https://yusuol.com">Yusuol</a>',
+      message: '© 2024-Present <a href="https://冀望.com">冀望</a>',
     },
     docFooter: {
       prev: '上一篇',
