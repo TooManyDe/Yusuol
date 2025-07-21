ca7bd2b85cb1ef1088735f3c09376b2c2acbdc9d
yusuol <jihaoqi@yahoo.com>
2025-07-21 09:03:24 +0800

    更新 shared.ts

--- docs/.vitepress/config/shared.ts
+++ docs/.vitepress/config/shared.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   cleanUrls: true,
   ignoreDeadLinks: true,
   sitemap: {
-    hostname: 'https://yusuol.com'
+    hostname: 'https://www.xn--q6qq04c.com/'
   },
   head: [
     ["script", { async: "", src: "https://www.googletagmanager.com/gtag/js?id=G-MB7XVBG1TQ" }],
