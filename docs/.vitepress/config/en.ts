0c52866300583f92c592b89929567e0ab602e6bf
yusuol <jihaoqi@yahoo.com>
2025-07-21 09:01:23 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -3,7 +3,7 @@ import { defineConfig} from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Never",
-  description: "",
+  description: "Never Resting Day nor Night",
   lang: "en-US", //语言
 
   themeConfig: {
