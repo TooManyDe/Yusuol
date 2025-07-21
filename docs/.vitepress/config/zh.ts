6416ff3ab992b989914d3974e9d961dd866f94d6
yusuol <jihaoqi@yahoo.com>
2025-07-21 08:15:23 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -2,7 +2,7 @@ import { type DefaultTheme, defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "今夕何夕",
+  title: "",
   description: "",
   lang: "zh-Hans", //语言
 
