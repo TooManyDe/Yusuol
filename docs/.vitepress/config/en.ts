fbd15690a7896b787c31f58b6877786d8b59fe49
yusuol <jihaoqi@yahoo.com>
2025-08-03 00:51:23 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 			{ text: "Sponsor", link: "/en/support-me", activeMatch: '/en/support-me' },
     ],
     footer: {
-      message: '<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND</a>© 2024-Present <a href="https://的的不休.com">冀望</a>',
+      message: '<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND</a> © 2024-Present <a href="https://的的不休.com">冀望</a>',
     },
 
     socialLinks: [
