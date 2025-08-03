8dafeb9cde282f48c6e37fc86959d6894d71cfa7
yusuol <jihaoqi@yahoo.com>
2025-08-03 14:09:45 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -2,7 +2,7 @@ import { type DefaultTheme, defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "DDBX",
+  title: "NEVER",
   description: "不舍昼夜",
   lang: "zh-Hans", //语言
 
