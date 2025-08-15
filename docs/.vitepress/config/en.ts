a9653954069566385e0704ad3bc695c60f7b5901
yusuol <jihaoqi@yahoo.com>
2025-08-16 03:25:17 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -2,7 +2,7 @@ import { defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Never",
+  title: "SKY",
   description: "Never Resting Day nor Night",
   lang: "en-US", //语言
 
