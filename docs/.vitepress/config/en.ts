1467ebb0108ca9c796e3313fad6dc3a60933c254
yusuol <jihaoqi@yahoo.com>
2025-08-16 02:08:27 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -2,7 +2,7 @@ import { defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "DDbx",
+  title: "never",
   description: "Never Resting Day nor Night",
   lang: "en-US", //语言
 
