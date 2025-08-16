0bd1c2a128fb9a4c7d0c1e5184cc24ec09b6109a
yusuol <jihaoqi@yahoo.com>
2025-08-16 23:49:41 +0800

    更新 en.ts

--- docs/.vitepress/config/en.ts
+++ docs/.vitepress/config/en.ts
@@ -2,7 +2,7 @@ import { defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Whisper",
+  title: "SkyWhisper",
   description: "Never Resting Day nor Night",
   lang: "en-US", //语言
 
