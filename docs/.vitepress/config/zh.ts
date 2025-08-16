6b379da1a8ab09567beedc0f75adf5a1da69f5ad
yusuol <jihaoqi@yahoo.com>
2025-08-16 09:30:41 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -2,7 +2,7 @@ import { type DefaultTheme, defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "SKY",
+  title: "SW",
   description: "不舍昼夜",
   lang: "zh-Hans", //语言
 
