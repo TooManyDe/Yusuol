63511b9a67cbc2a9278f9599bc88ba57590aab77
yusuol <jihaoqi@yahoo.com>
2025-08-16 23:46:39 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -2,7 +2,7 @@ import { type DefaultTheme, defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "SYWR",
+  title: "Never",
   description: "不舍昼夜",
   lang: "zh-Hans", //语言
 
