e95feb7169b947d7a08c51cdfeac28c56136d1e4
yusuol <jihaoqi@yahoo.com>
2025-08-24 01:39:27 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -1,5 +1,4 @@
 import { type DefaultTheme, defineConfig } from 'vitepress'
-import posts from './sidebar'
 
 // RSS 图标 SVG
 const RSS_ICON_SVG = '<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>RSS</title><path d="M108.56,342.78a60.34,60.34,0,1,0,60.56,60.44A60.63,60.63,0,0,0,108.56,342.78Z"/><path d="M48,186.67v86.55c52,0,101.94,15.39,138.67,52.11s52,86.56,52,138.67h86.66C325.33,312.44,199.67,186.67,48,186.67Z"/><path d="M48,48v86.56c185.25,0,329.22,144.08,329.22,329.44H464C464,234.66,277.67,48,48,48Z"/></svg>'
@@ -67,35 +66,24 @@ export const search: DefaultTheme.AlgoliaSearchOptions['locales'] = {
 // 主题配置
 const themeConfig: DefaultTheme.Config = {
   nav: navConfig,
-
-  sidebar: [
-    {
-      text: '最新文章',
-      items: posts.map(post => ({
-        text: post.frontmatter.title || post.url.split('/').pop(),
-        link: post.url
-      }))
-    }
-  ],
-
+  
   footer: {
     message: '<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">CC BY-NC-ND 4.0</a> © 2021-2025 <a href="https://skywhisper.org">SkyWhisper</a>',
   },
-
+  
   docFooter: {
     prev: '上一篇',
     next: '下一篇'
   },
-
+  
   outlineTitle: "当前页面",
   lastUpdatedText: "最近更新",
   returnToTopLabel: "回到顶部",
   sidebarMenuLabel: "目录",
   darkModeSwitchLabel: "深色模式",
-
+  
   socialLinks: socialLinksConfig,
-},
-
+}
 
 // 主配置
 export default defineConfig({
