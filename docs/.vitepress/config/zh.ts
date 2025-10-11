2c87cb44bcff8923da26bc415199a4b301ceda2c
yusuol <jihaoqi@yahoo.com>
2025-10-12 03:13:35 +0800

    更新 zh.ts

--- docs/.vitepress/config/zh.ts
+++ docs/.vitepress/config/zh.ts
@@ -18,50 +18,34 @@ const socialLinksConfig: DefaultTheme.SocialLink[] = [
   },
 ]
 
-// 搜索本地化配置
-export const search: DefaultTheme.AlgoliaSearchOptions['locales'] = {
-  root: {
-    placeholder: '搜索',
-    translations: {
-      button: {
-        buttonText: '搜索',
-        buttonAriaLabel: '搜索'
-      },
-      modal: {
-        searchBox: {
-          resetButtonTitle: '清除查询条件',
-          resetButtonAriaLabel: '清除查询条件',
-          cancelButtonText: '取消',
-          cancelButtonAriaLabel: '取消'
-        },
-        startScreen: {
-          recentSearchesTitle: '搜索历史',
-          noRecentSearchesText: '无搜索历史',
-          saveRecentSearchButtonTitle: '保存至搜索历史',
-          removeRecentSearchButtonTitle: '从搜索历史中移除',
-          favoriteSearchesTitle: '收藏',
-          removeFavoriteSearchButtonTitle: '从收藏中移除'
-        },
-        errorScreen: {
-          titleText: '无法获取结果',
-          helpText: '网络错误'
-        },
-        footer: {
-          selectText: '选择',
-          navigateText: '切换',
-          closeText: '关闭',
-          searchByText: '搜索来源'
-        },
-        noResultsScreen: {
-          noResultsText: '无相关结果',
-          suggestedQueryText: '你可以尝试查询',
-          reportMissingResultsText: '你认为该查询应该有结果？',
-          reportMissingResultsLinkText: '点击反馈'
+
+export default defineConfig({
+  themeConfig: {
+    search: {
+      provider: 'local',
+      options: {
+        locales: {
+          zh: {
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                noResultsText: '无法找到相关结果',
+                resetButtonTitle: '清除查询条件',
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换'
+                }
+              }
+            }
+          }
         }
       }
     }
   }
-}
+})
 
 // 主题配置
 const themeConfig: DefaultTheme.Config = {
