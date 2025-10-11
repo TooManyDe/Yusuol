0bdf48c8b223f7f1133d5b36e2d9aece435a4dcb
yusuol <jihaoqi@yahoo.com>
2025-10-12 00:41:29 +0800

    更新 index.ts

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,35 +6,35 @@ import 'tdesign-vue-next/es/style/index.css';
 
 import "./style.css";
 import Comment from "./components/Comment.vue";
-import ImageViewer from "./components/ImageViewer.vue"
+import ImageViewer from "./components/ImageViewer.vue";
+import Layout from "./Layout.vue"; // 👈 新增：导入自定义 Layout
 
 export default {
- sidebar: false,
-	...Theme,
-	Layout: () => {
-		return h(Theme.Layout, null, {
-			// https://vitepress.dev/guide/extending-default-theme#layout-slots
-			"doc-after": () => h(Comment),
-			"doc-top": () => h(ImageViewer),
-		});
-	},
+  sidebar: false,
+  ...Theme,
+  Layout: () => {
+    return h(Layout, null, { // 👈 修改：使用自定义 Layout 替代 Theme.Layout
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      "doc-after": () => h(Comment),
+      "doc-top": () => h(ImageViewer),
+    });
+  },
 
-	enhanceApp({ app, router }: any) {
-		app.component("Comment", Comment);
-		app.component("ImageViewer", ImageViewer);
+  enhanceApp({ app, router }: any) {
+    app.component("Comment", Comment);
+    app.component("ImageViewer", ImageViewer);
 
-		router.onAfterRouteChanged = (to: string) => {
-			// 兼容旧博客的中文路径，重定向到新路径，避免外链失效
-			if (to.startsWith(encodeURI('/博客/'))) {
-				const newUrl = to.replace(encodeURI('/博客/'), '/posts/')
-				window.location.href = newUrl
-			}
+    router.onAfterRouteChanged = (to: string) => {
+      // 兼容旧博客的中文路径，重定向到新路径，避免外链失效
+      if (to.startsWith(encodeURI('/博客/'))) {
+        const newUrl = to.replace(encodeURI('/博客/'), '/posts/')
+        window.location.href = newUrl
+      }
 
-			if (to.startsWith(encodeURI('/笔记/'))) {
-				const newUrl = to.replace(encodeURI('/笔记/'), '/notes/')
-				window.location.href = newUrl
-			}
-		}
-	},
+      if (to.startsWith(encodeURI('/笔记/'))) {
+        const newUrl = to.replace(encodeURI('/笔记/'), '/posts/')
+        window.location.href = newUrl
+      }
+    }
+  },
 };
-
