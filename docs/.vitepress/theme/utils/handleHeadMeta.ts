1e27a5960e7198f8406be5aaa7a62d56252decb1
yusuol <jihaoqi@yahoo.com>
2025-10-16 15:40:46 +0800

    更新 handleHeadMeta.ts

--- docs/.vitepress/theme/utils/handleHeadMeta.ts
+++ docs/.vitepress/theme/utils/handleHeadMeta.ts
@@ -5,7 +5,7 @@ export function handleHeadMeta(context: TransformContext) {
   const { description, title, relativePath, frontmatter } = context.pageData;
 
   const curDesc = description || context.description;
-  const cover = frontmatter.cover || 'https://skywhisper.org/bg.jpg'
+  const cover = frontmatter.cover || 'https://ddbx.org/bg.jpg'
   const cardType = frontmatter.cover ? 'summary_large_image' : 'summary'
   // 增加 Twitter 卡片
   const ogUrl: HeadConfig = ["meta", { property: "og:url", content: addBase(relativePath) }]
@@ -28,7 +28,7 @@ export function handleHeadMeta(context: TransformContext) {
 }
 
 export function addBase(relativePath: string) {
-  const host = 'https://skywhisper.org/'
+  const host = 'https://ddbx.org/'
   if (relativePath.startsWith('/')) {
     return host + relativePath
   } else {
