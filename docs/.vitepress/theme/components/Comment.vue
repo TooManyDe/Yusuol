844e09ed314b1a57c227be9a8ed2c257be0b21c9
yusuol <jihaoqi@yahoo.com>
2026-01-08 04:56:13 +0800

    更新 Comment.vue

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -3,7 +3,7 @@
     <span
       class="comment-toggle"
       @click="toggleComments"
-      style="color: #41b349; font-family: 'Noto Serif SC'; font-weight: 600"
+      style="color: #41b349; font-family: 'Noto Serif SC'; font-weight: 600;"
     >
       {{ loadComments ? commentText : commentText }}
     </span>
