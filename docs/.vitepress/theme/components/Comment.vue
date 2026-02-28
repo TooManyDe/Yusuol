6af14bb4190ad5a47b8a614c35a419d11326223e
TooManyDe <too.many.de@gmail.com>
2026-02-28 19:57:34 +0800

    更新 Comment.vue

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -3,7 +3,7 @@
     <span
       class="comment-toggle"
       @click="toggleComments"
-      style="color: #326891; font-family: 'Noto Serif SC'; font-weight: 500;"
+      style="color: #41b349; font-family: 'Noto Serif SC'; font-weight: 500;"
     >
       {{ loadComments ? commentText : commentText }}
     </span>
